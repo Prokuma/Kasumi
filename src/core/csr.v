148adefbc649da1ca69cbf9910e1c5805667b4b8
Prokuma <prokuma@prokuma.dev>
2022-06-14 08:08:01 +0900

    [Update] CSRs

--- src/core/csr.v
+++ src/core/csr.v
@@ -14,14 +14,17 @@ assign out_trap_vec = csr_register[12'h305];
 assign out_exception_pc = csr_register[12'h341];
 
 always @(*) begin
-    if (wb_csr)
+    if (wb_csr & (write_addr[11:10] != 2'b11))
         csr_register[write_addr] <= in_data;
 end
 
 integer i;
 initial begin
     for(i = 0; i < 4096; i = i + 1) begin
-        csr_register[i] = 32'b0;
+        case(i)
+            12'h301: csr_register[i] = 32'b01_0000_0000000000000000100000000;
+            default: csr_register[i] = 32'b0;
+        endcase
     end
 end
 
